5a974ce2d0dcc00f106d014781c5853b21daea92
Abiy Addis <abiymw17@gmail.com>
2024-08-04 21:14:51 +0300

    sd

--- public/script.js
+++ public/script.js
@@ -1,32 +1,3 @@
-let underline = document.querySelectorAll(".underline-effect");
-
-// The underline effect for the navbar so when you click it, it stays on a single <a>
-underline.forEach(underLine=>{
-    underLine.addEventListener('click', function (e){
-    underline.forEach(underLine =>{
-        underLine.classList.remove('effect');
-    });
-    this.classList.add('effect');
-    });
-});
-
-const sections = document.querySelectorAll('.section');
-window.addEventListener('scroll',()=>{
-    const scrollY = window.scrollY ; //the distance of how long the window moved
-
-    sections.forEach((section,index) =>{
-        const sectionTop = section.offsetTop;
-        // the distance between the top of the section to the top of the parent(body in this instance)
-        if(scrollY>=sectionTop){
-            underline.forEach(underLine=>{
-                underLine.classList.remove('effect');
-            });
-            underline[index].classList.add('effect');
-        }
-    });
-});
-
-
 
 //for screens less than 768px for the navbar to be active when clicked on the menu btn
 header3=document.getElementById("menu_btn");
